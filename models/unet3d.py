f7b273183790f8a478145fd167cad458d2861392
guillesanbri <guillesanbri@hotmail.com>
2025-05-02 18:24:04 +0000

    use list of features instead of features + depth to increase flexibility

--- models/unet3d.py
+++ models/unet3d.py
@@ -1,4 +1,4 @@
-from typing import Optional, Callable, Tuple, Union
+from typing import Optional, Callable, Tuple, Union, List
 
 import torch
 import torch.nn as nn
@@ -69,32 +69,26 @@ class UNet3D(nn.Module):
     Input/output shape: [Batch, Channels, Time/depth, Height, Width]
     """
 
-    # TODO: Compare both bilinear interpolation and transpose convolutions to upscale
     # TODO: Compare batchnorm to group norm
-    # TODO: Compare with and without downsampling the time dimension
     # n_classes should be 1 if doing regression + MSE
     # n_classes should be 256 if each pixel is a classification problem (CE)
     def __init__(
         self,
         in_channels: int,
-        num_classes: int = 1,
-        depth: int = 4,
-        first_layer_features: int = 32,
+        num_classes: int,
+        features: List[int],
         conv_kernel_size: int = 3,
         pool_kernel_size: int = 2,
         final_activation: Optional[Callable] = None,
     ):
         super().__init__()
+        self.features = [in_channels, *features]
 
         _encoder_blocks = []
-        for i in range(depth):
-            if i == 0:
-                in_c = in_channels
-                downsample = False
-            else:
-                in_c = first_layer_features * (2 ** (i - 1))
-                downsample = True
-            out_c = first_layer_features * (2**i)
+        for i in range(len(self.features) - 1):
+            downsample = i != 0
+            in_c = self.features[i]
+            out_c = self.features[i + 1]
             _encoder_blocks.append(
                 EncoderBlock(
                     in_c, out_c, conv_kernel_size, pool_kernel_size, downsample
@@ -103,16 +97,12 @@ class UNet3D(nn.Module):
         self.encoder_blocks = nn.ModuleList(_encoder_blocks)
 
         _decoder_blocks = []
-        for i in range(depth - 1, 0, -1):
-            in_c = int(
-                (first_layer_features * (2**i))
-                + (first_layer_features * (2 ** (i - 1)))
-            )
-            out_c = int(first_layer_features * (2 ** (i - 1)))
-            _decoder_blocks.append(DecoderBlock(in_c, out_c, conv_kernel_size))
+        for i in range(len(self.features) - 1, 1, -1):
+            in_c = self.features[i]
+            out_c = self.features[i - 1]
         self.decoder_blocks = nn.ModuleList(_decoder_blocks)
 
-        self.final_conv = nn.Conv3d(first_layer_features, num_classes, 1)
+        self.final_conv = nn.Conv3d(self.features[1], num_classes, 1)
         self.final_activation = final_activation
 
     def forward(self, x):
@@ -134,6 +124,12 @@ class UNet3D(nn.Module):
 if __name__ == "__main__":
     device = "cuda"
     x = torch.rand(1, 1, 10, 128, 128).to(device)
-    unet = UNet3D(1, 1, pool_kernel_size=(2, 2, 2)).to(device)
+    unet = UNet3D(
+        1,
+        1,
+        features=[32, 64, 128, 256],
+        pool_kernel_size=(2, 2, 2),
+        final_activation=nn.Sigmoid(),
+    ).to(device)
     y = unet(x)
     print(y.shape)
